a917cc58fea67cc324c74f4b042e3e0153862927
kts123 <kts123@estsoft.com>
2021-09-30 00:01:05 +0900

    front mask

--- src/mask_history.py
+++ src/mask_history.py
@@ -517,6 +517,111 @@ def calc_poly_pwb_front_v39_0(pts, h, randomness=True):
         
         
     return np.round(np.array(pts)).astype(np.int32)
+# 박은보, 옆모습, 아주 많이 가림
+def calc_poly_pwb_side_v39(pts, h, randomness=True):
+    
+    def rand(a, b):
+        if randomness:
+            r = random.random()
+            return a + (b-a)*r
+        else:
+            return (a+b)/2
+    
+    p0 = pts[27]
+    y10 = h*0.85
+    y6 =  y10
+    y8 =  y10
+    
+    p15 = pts[15] + rand(0.01, 0.05)*(pts[15] - p0)#- rand(0.01, 0.10)*(pts[15] - p0)
+    p13 = pts[13] + rand(0.01, 0.05)*(pts[13] - pts[30])#- rand(0.01, 0.10)*(pts[13] - pts[30])
+    p12 = pts[12] + rand(0.05, 0.10)*(pts[12] - pts[30])#- rand(0.01, 0.05)*(pts[12] - pts[30])
+    p11 = pts[11] + rand(0.18, 0.22)*(pts[11] - pts[30])#+ rand(0.18, 0.22)*(pts[11] - pts[30])
+    p11[0] = pts[11][0]
+    p10 = [pts[10][0], y10]
+    p08 = [pts[9][0], y8]
+    p06 = [pts[8][0], y6]
+    p05 = pts[5] + rand(0.18, 0.22)*(pts[5] - pts[30])
+    p05[0] = pts[5][0]
+    p04 = pts[4] + rand(0.01, 0.05)*(pts[4] - pts[30])
+    p03 = pts[3] + rand(0.01, 0.10)*(pts[3] - pts[30])
+    p01 = pts[1] - rand(0.01, 0.10)*(pts[1] - p0)
+    
+    #pts = [p0, p15, p13, p12, p11, p10, p08, p06, p05, p04, p03, p01]
+    pts = [p0, p15, p13, p12, p10, p08, p06, p05, p04, p03, p01]
+    return np.round(np.array(pts)).astype(np.int32)
+
+
+# 박은보, 옆모습, 많이 가리지 않음, 예전 v9와 유사
+def calc_poly_pwb_side_v39_1(pts, h, randomness=True):
+    
+    def rand(a, b):
+        if randomness:
+            r = random.random()
+            return a + (b-a)*r
+        else:
+            return (a+b)/2
+        
+    p0 = pts[27]
+    y10 = h*0.85
+    y6 =  y10
+    y8 =  y10
+    
+    p15 = pts[15] - rand(0.01, 0.10)*(pts[15] - p0)
+    p13 = pts[13] - rand(0.01, 0.10)*(pts[13] - pts[30])
+    p12 = pts[12] - rand(0.01, 0.05)*(pts[12] - pts[30])
+    p11 = pts[11] + rand(0.18, 0.22)*(pts[11] - pts[30])
+    p11[0] = pts[11][0]
+    p10 = [pts[10][0], y10]
+    p08 = [pts[9][0], y8]
+    p06 = [pts[8][0], y6]
+    p05 = pts[5] + rand(0.18, 0.22)*(pts[5] - pts[30])
+    p05[0] = pts[5][0]
+    p04 = pts[4] - rand(0.01, 0.05)*(pts[4] - pts[30])
+    p03 = pts[3] - rand(0.01, 0.10)*(pts[3] - pts[30])
+    p01 = pts[1] - rand(0.01, 0.10)*(pts[1] - p0)
+    
+    #pts = [p0, p15, p13, p12, p11, p10, p08, p06, p05, p04, p03, p01]
+    pts = [p0,  
+           p15, p13, p12, p10, p08, p06, p05, p04, p03, p01]
+    return np.round(np.array(pts)).astype(np.int32)
+
+def calc_poly_pwb_front_v39_1(pts, h, randomness=True):
+    
+    def rand_(a, b, randomness=True):
+        if randomness:
+            r = random.random()
+            return a + (b-a)*r
+        else:
+            return (a+b)/2
+        
+    def rand(s, e):
+        return rand_(s, e, randomness)
+    
+    p0 = [pts[27][0], pts[27][1] + h*0.09]
+    #p0 = pts[27]
+    y10 = h*0.9
+    y6 =  y10
+    y8 =  y10
+    
+    p15 = pts[15] - rand(0.02, 0.06)*(pts[15] - p0)
+    p12 = [p15[0], p15[1] + (y10 - p15[1])*0.5] 
+    
+    
+    p10 = [h*0.65, y10]
+    p08 = [h*0.5, y8]
+    p06 = [h*0.35, y6]
+    
+    p01 = pts[1] - rand(0.01, 0.10)*(pts[1] - p0)
+    p04 = [p01[0], p01[1] + (y10-p01[1])*0.5]
+    
+    
+    pts = [p0,  # 미간
+           p15, p12, # 우리가 보기에 오른쪽, p15가 위
+           p10, p08, p06, # 맨 아래 
+           p04, p01] # 우리가 보기에 왼쪽, p01 이 위, p04가 아래
+        
+        
+    return np.round(np.array(pts)).astype(np.int32)
 
 calc_poly = {
     6:calc_poly_v6,
@@ -531,5 +636,6 @@ calc_poly = {
     24:calc_poly_v24,
     'pwb_side_v39': calc_poly_pwb_side_v39,
     'pwb_side_v39_1': calc_poly_pwb_side_v39_1,
-    'pwb_front_v39_0': calc_poly_pwb_front_v39_0
+    'pwb_front_v39_0': calc_poly_pwb_front_v39_0,
+    'pwb_front_v39_1': calc_poly_pwb_front_v39_1
 }
